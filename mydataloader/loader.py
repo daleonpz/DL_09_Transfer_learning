d1ac749eb27ae37f969cbd9f614399220b44aa28
daniel.paredes <daniel.paredes@q-loud.de>
2022-12-13 19:15:17 +0100

    reformat

--- mydataloader/loader.py
+++ mydataloader/loader.py
@@ -6,7 +6,7 @@ from medmnist import INFO
 from torchvision import transforms as T
 
 # Specify dataset
-DEVICE      = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 DOWNLOAD_FLAG = True
 
 # batch_size = 256
@@ -18,24 +18,30 @@ DOWNLOAD_FLAG = True
 
 os.makedirs("./figs/", exist_ok=True)
 
+
 class DataLoader:
     def __init__(self):
         # Moves the range [0,1] to [-1,1]
-        self.mean    = torch.tensor([0.5], dtype=torch.float32)
-        self.std     = torch.tensor([0.5], dtype=torch.float32)
+        self.mean = torch.tensor([0.5], dtype=torch.float32)
+        self.std = torch.tensor([0.5], dtype=torch.float32)
 
-        self.plain_transform = T.Compose([T.ToTensor(), T.Normalize(list(mean), list(std))])
+        self.plain_transform = T.Compose(
+            [T.ToTensor(), T.Normalize(list(self.mean), list(self.std))]
+        )
 
-        DATA_FLAG   = 'pathmnist'
+        DATA_FLAG = "pathmnist"
         info = INFO[DATA_FLAG]
-        self.DataClass = getattr(medmnist, info['python_class'])
-
+        self.DataClass = getattr(medmnist, info["python_class"])
 
     def load_datasets(self):
-        train_ds_plain  = self.DataClass(split='train',  transform=self.plain_transform, download=DOWNLOAD_FLAG)
-        val_ds          = self.DataClass(split='val',    transform=self.plain_transform, download=DOWNLOAD_FLAG)
-        test_ds         = self.DataClass(split='test',   transform=self.plain_transform, download=DOWNLOAD_FLAG)
+        train_ds_plain = self.DataClass(
+            split="train", transform=self.plain_transform, download=DOWNLOAD_FLAG
+        )
+        val_ds = self.DataClass(
+            split="val", transform=self.plain_transform, download=DOWNLOAD_FLAG
+        )
+        test_ds = self.DataClass(
+            split="test", transform=self.plain_transform, download=DOWNLOAD_FLAG
+        )
 
         return train_ds_plain, val_ds, test_ds
-
-
