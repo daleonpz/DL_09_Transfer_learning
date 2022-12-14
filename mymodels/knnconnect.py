9052b7bcd2d320b0c906b44e4c64aa0146e7b2a2
daniel.paredes <daniel.paredes@q-loud.de>
2022-12-14 16:21:52 +0100

    update

--- mymodels/knnconnect.py
+++ mymodels/knnconnect.py
@@ -89,7 +89,7 @@ class KnnConvnet:
         ### START CODE HERE ### (approx. 2 lines)
         prediction = self.knnClassifier.predict(features.to('cpu'))
         print(f'prediction: {prediction} ')
-        print(f'labels: {labels}')
+        print(f'labels: {torch.t(labels)}')
         acc = (torch.tensor(prediction) == labels).sum().item() / labels.shape[0] 
 #         acc = torch.norm( torch.tensor(prediction) - labels ) # tensor - tensor
 #         acc = cross_val_score( self.knnClassifier, prediction.reshape(-1,1),  labels.ravel(), 
